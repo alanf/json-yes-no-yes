13b4a0732c845c0ebd83c33c3e751614c6d82447
audioair <audioair@f7dbd029-c475-4d9f-af7a-de61342244e9>
2010-02-11 02:56:31 +0000

    resolve conflicts
    git-svn-id: https://subversion.assembla.com/svn/yes-no-yes@17 f7dbd029-c475-4d9f-af7a-de61342244e9

--- js/rules.js
+++ js/rules.js
@@ -1,68 +1,68 @@
 rules = {
-	'start': [
-				{
-				'type': 'image',
-				'next': 'second',
-				'content': 'start.jpg',
-				'position': {'x': '10px', 'y': '50px'},
-				'width': '9%'
-				},
-				{
-				'type': 'image',
-				'next': 'third',
-				'content': 'start.jpg',
-				'position': {'x': '85px', 'y': '267px'}
-				},
-				{
-				'type': 'image',
-				'next': 'third',
-				'content': 'start.jpg',
-				'position': {'x': '585px', 'y': '180px'}
-				},
-				{
-				'type': 'image',
-				'next': 'third',
-				'content': 'start.jpg',
-				'position': {'x': '400px', 'y': '300px'}
-				},
-				{
-				'type': 'image',
-				'next': 'third',
-				'content': 'start.jpg',
-				'position': {'x': '845px', 'y': '387px'}
-				}
-	],
-	'second': [
-				{
-				'type': 'image',
-				'next': 'start',
-				'content': 'trabant.jpg',
-				'position': {'x': '55px', 'y': '167px'}
-				},
-				{
-				'type': 'text',
-				'content': 'this is some other example text',
-				'position': {'x': '669px', 'y': '528px'}
-				}
-	],
-	'third': [
-				{
-				'type': 'image',
-				'next': 'start',
-				'content': 'puppy.jpg',
-				'position': {'x': '998px', 'y': '167px'}
-				},
-				{
-				'type': 'image',
-				'next': 'second',
-				'content': 'avs.jpg',
-				'position': {'x': '155px', 'y': '0px'}
-				},
-				{
-				'type': 'text',
-				'content': 'whoa this is another thing',
-				'position': {'x': '69px', 'y': '628px'}
-				}
-	]
+    'start': [
+                {
+                'type': 'image',
+                'next': 'second',
+                'content': 'start.jpg',
+                'position': {'x': '50px', 'y': '50px'}
+                'width': '100%'
+                },
+                {
+                'type': 'image',
+                'next': 'third',
+                'content': 'start.jpg',
+                'position': {'x': '185px', 'y': '425px'}
+                },
+                {
+                'type': 'image',
+                'next': 'third',
+                'content': 'start.jpg',
+                'position': {'x': '900px', 'y': '110px'}
+                },
+                {
+                'type': 'image',
+                'next': 'third',
+                'content': 'start.jpg',
+                'position': {'x': '660px', 'y': '170px'}
+                },
+                {
+                'type': 'image',
+                'next': 'third',
+                'content': 'start.jpg',
+                'position': {'x': '845px', 'y': '387px'}
+                }
+    ],
+    'second': [
+                {
+                'type': 'image',
+                'next': 'third',
+                'content': 'page2_img1.jpg',
+                'position': {'x': '10px', 'y': '30px'}
+                },
+                {
+                'type': 'image',
+                'next': 'third',
+                'content': 'page2_img2.jpg',
+                'position': {'x': '65px', 'y': '85px'}
+                },
+    'third': [
+                {
+                'type': 'image',
+                'next': 'start',
+                'content': 'puppy.jpg',
+                'position': {'x': '998px', 'y': '167px'}
+                },
+                {
+                'type': 'image',
+                'next': 'second',
+                'content': 'avs.jpg',
+                'position': {'x': '155px', 'y': '0px'}
+                },
+                {
+                'type': 'text',
+                'content': 'whoa this is another thing',
+                'position': {'x': '69px', 'y': '628px'}
+                }
+    ]
 }
 
