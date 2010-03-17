01aa1c0966627808b16a400c0e208ae19c54fdc8
uniquesnowflake8 <uniquesnowflake8@f7dbd029-c475-4d9f-af7a-de61342244e9>
2010-03-17 05:40:33 +0000

    hide home button.
    git-svn-id: https://subversion.assembla.com/svn/yes-no-yes@99 f7dbd029-c475-4d9f-af7a-de61342244e9

--- js/controller.js
+++ js/controller.js
@@ -5,8 +5,9 @@
 
 var $ = goog.dom.$;
 var $$ = goog.dom.$$;
-var DEBUG_BOX = true;
-var BACK_BUTTON = true;
+var SHOW_DEBUG_BOX = false;
+var SHOW_BACK_BUTTON = false;
+var SHOW_HOME_BUTTON = false;
 
 get_rules_from_id = function(id) {
 	var prev = id.split('-')[1];
@@ -144,21 +145,17 @@ show_page = function(name, prev, from_redirect) {
 		$('main-body').appendChild(new_element);	
 	});
 
-	// These links are always present on the page
-	var debug_back_img = goog.dom.createDom('img', {'src': 'images/back.jpg', 'id': 'debug-'+prev+'-'+prev});
-	var start_img = goog.dom.createDom('img', {'src': 'images/home.jpg', 'id': 'home'});
-	var links = [start_img];
-	if (BACK_BUTTON) {
-		links.push(debug_back_img);
+	if (SHOW_BACK_BUTTON) {
+		var debug_back_img = goog.dom.createDom('img', {'src': 'images/back.jpg', 'id': 'debug-'+prev+'-'+prev});
 		$('main-body').appendChild(debug_back_img);	
 		goog.events.listen(debug_back_img, goog.events.EventType.CLICK, main_action, false, this);
 	}
-
-	
-	$('main-body').appendChild(start_img);	
-	goog.events.listen(start_img, goog.events.EventType.CLICK, function() { return show_page('init', 'start');}, false, this);
-
-	if (DEBUG_BOX) {
+	if (SHOW_HOME_BUTTON) {
+		var start_img = goog.dom.createDom('img', {'src': 'images/home.jpg', 'id': 'home'});
+		$('main-body').appendChild(start_img);	
+		goog.events.listen(start_img, goog.events.EventType.CLICK, function() { return show_page('init', 'start');}, false, this);
+	}
+	if (SHOW_DEBUG_BOX) {
 		var debug_input = goog.dom.createDom('input', {'type': 'text', 'id': 'debug-input'});
 		$('main-body').appendChild(debug_input);	
 		goog.events.listen(debug_input, goog.events.EventType.KEYUP, alter_debug_jump_link, false, this);
